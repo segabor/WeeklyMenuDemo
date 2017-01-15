f4013afed6e097c4ef2469c1519fa20bac4b6e90
Gábor SEBESTYÉN <segabor@gmail.com>
2017-01-15 23:26:57 +0100

    Sample database with data

---
+++ db/01_create_database.sql
@@ -0,0 +1,3 @@
+create database menu default character set utf8;
+grant all on menu.* to 'menu'@'%' identified by 'menu';
+
---
+++ db/02_create_tables.sql
@@ -0,0 +1,4 @@
+create table menu(id int NOT NULL AUTO_INCREMENT, title varchar(32) not null, subtitle varchar(64) not null, description varchar(1024) not null, imgUrl varchar(256) not null, primary key(id));
+
+-- test data
+insert into menu(title, subtitle, description, imgUrl) values('paprikás ccsirke', 'Csirke paprikás magyaros módon galuska körettel', 'Leírás', '/images/csirke.png');
