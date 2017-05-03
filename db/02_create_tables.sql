a66513299fa3fabeac4b6d7d4824878dfa045ed9
Gábor Sebestyén <segabor@gmail.com>
2017-05-03 13:07:33 +0200

    Fix DB data populator script

--- db/02_create_tables.sql
+++ db/02_create_tables.sql
@@ -1,3 +1,4 @@
+use menu;
 create table menu(id int NOT NULL AUTO_INCREMENT, title varchar(32) not null, subtitle varchar(64) not null, description varchar(1024) not null, imgUrl varchar(256) not null, primary key(id));
 
 -- test data
