9a408f6d856cf67f95d2091a76091f566be07be8
Gábor Sebestyén <segabor@gmail.com>
2017-05-03 13:09:00 +0200

    Misc changes
    - bump Swift version to 3.1.1
    - allow fresh Packages

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
     ],
 
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
-      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4),
-      .Package(url: "https://github.com/vapor/mysql", majorVersion: 1, minor: 0),
-      .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 9)
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
+      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
+      .Package(url: "https://github.com/vapor/mysql", majorVersion: 1),
+      .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1)
     ]
 )
